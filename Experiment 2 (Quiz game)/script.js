e07ec0623dac7d157e51c850d67b887d79fb04ec
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-24 17:11:11 +0530

    Commmit

--- Experiment 2 (Quiz game)/script.js
+++ Experiment 2 (Quiz game)/script.js
@@ -1,193 +1,142 @@
-let n1;
-let n2;
-let opSelector;
-let ansOpt;
-let answer;
+let n1, n2, opSelector, ansOpt, answer, t;
+
+// Elements
 let qNo = document.getElementById("Qno");
 let score = document.getElementById("score");
 let question = document.getElementById("question");
 let buttons = document.getElementsByTagName("button");
-let start = document.getElementById("start-btn");
-let fScore = document.getElementById("final-score");
 let startBox = document.getElementById("start-game");
 let gameBox = document.getElementById("in-game");
 let endBox = document.getElementById("end-game");
 let progress = document.getElementById("progress");
+let fScore = document.getElementById("final-score");
 let message = document.getElementById("message");
 let operator = ['+', '-', '*', '/'];
-let t;
 
+// Restart the game
 function restart() {
     score.innerHTML = "0";
     qNo.innerHTML = "0";
     nextQuestion();
 
-    gameBox.style.display = "block"
+    gameBox.style.display = "block";
     startBox.style.display = "none";
     endBox.style.display = "none";
-    timer.style.display = "block";
+    progress.style.width = "100%";
 }
 
+// When the game is finished
 function whenFinished() {
-    console.log("Finished.")
-    gameBox.style.display = "none"
+    clearInterval(t);
+    gameBox.style.display = "none";
     startBox.style.display = "none";
     endBox.style.display = "flex";
-    lastmessage();
+
+    fScore.innerHTML = score.innerHTML; // Show the final score
+    lastMessage(); // Show motivational message
 }
 
+// Generate the next question
 function nextQuestion() {
+    clearInterval(t); // Clear any previous timer
+    progress.style.width = "100%"; // Reset progress bar
+    timed(); // Start a new timer
 
-    progress.style.width = "100%";
-    timed();
-    // timed();
-    fScore.innerHTML = score.innerHTML;
-    if (qNo.innerText == "10") {
+    if (qNo.innerText == "10") { // End the game after 10 questions
         whenFinished();
+        return;
     }
+
     n1 = Math.floor(Math.random() * 100);
     n2 = Math.floor(Math.random() * 100);
     opSelector = operator[Math.floor(Math.random() * 4)];
 
     if (opSelector == "/") {
-        for (let i = 0; i < 200; i++) {
-            if (n1 % n2 == 0 && n1 != 0 && n2 != 0 && n2 != 1 && n1 != n2) {
-                break;
-            }
+        while (n1 % n2 !== 0 || n1 === 0 || n2 === 0 || n2 === 1 || n1 === n2) {
             n1 = Math.floor(Math.random() * 100);
             n2 = Math.floor(Math.random() * 100);
         }
     }
 
     if (opSelector == "*") {
-        for (let i = 0; i < 100; i++) {
-            if (n1 * n2 <= 1000) {
-                break;
-            }
+        while (n1 * n2 > 1000) {
             n1 = Math.floor(Math.random() * 50);
             n2 = Math.floor(Math.random() * 50);
         }
     }
-    question.innerHTML = n1 + opSelector + n2;
-    answer = eval(question.innerHTML);
-    question.innerHTML = question.innerHTML + " = ?";
 
-    // console.log("answer: " + answer);
+    question.innerHTML = `${n1} ${opSelector} ${n2} = ?`;
+    answer = eval(`${n1} ${opSelector} ${n2}`);
     getOptions();
-    getQNo();
-
+    incrementQNo();
 }
 
+// Generate answer options
 function getOptions() {
-
-    for (let i = 0; i < 4; i++ && i != ansOpt) {
-        if (answer > 100) {
-            buttons[i].innerHTML = answer + Math.floor(Math.random() * answer * 0.4);
-        } else if (answer > 30 && answer < 100) {
-            buttons[i].innerHTML = answer + Math.floor(Math.random() * answer * 0.6);
-        } else {
-            buttons[i].innerHTML = Math.floor(Math.random() * 100);
-        }
-
-        if (answer < 0) {
-            buttons[i].innerHTML = "-" + buttons[i].innerHTML;
-        }
-    }
     ansOpt = Math.floor(Math.random() * 4);
     buttons[ansOpt].innerHTML = answer;
-}
 
-function getQNo() {
-    qNo.innerHTML = parseInt(qNo.innerHTML) + 1;
-    // console.log("Q no: " + qNo.innerHTML);
-}
-
-function getScore() {
-    score.innerHTML = parseInt(score.innerHTML) + parseInt(progress.style.width);
-    // console.log(score.innerHTML);
-}
-
-function doWhenCorrect(i) {
-    buttons[i].style.color = "#fff";
-    buttons[i].style.backgroundColor = "green";
-    getScore();
-}
-
-function doWhenIncorrect(i) {
-    buttons[i].style.color = "#fff";
-    buttons[i].style.backgroundColor = "#fb3640";
-    // console.log("wrong");
-}
-
-function outro(i) {
-    setTimeout(() => {
-        nextQuestion();
+    for (let i = 0; i < 4; i++) {
+        if (i !== ansOpt) {
+            buttons[i].innerHTML = Math.floor(Math.random() * 200) - 50;
+        }
         buttons[i].style.color = "#000";
         buttons[i].style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    }, 500);
-}
-
-function lastmessage() {
-    clearInterval(t);
-    if (fScore.innerText >= 800) {
-        let emoji = "&#128525";
-        message.innerHTML = "WOW !! UNBELIEVABLE !!" + emoji;
-    } else if (fScore.innerText >= 500) {
-        let emoji = "&#128531";
-        message.innerHTML = "TOO CLOSE !!" + emoji;
-    } else if (fScore.innerText >= 100) {
-        let emoji = "&#128549";
-        message.innerHTML = "Better luck next time " + emoji;
-    } else {
-        let emoji = "&#128577";
-        message.innerHTML = "Bad Luck " + emoji;
     }
 }
 
+// Increment question number
+function incrementQNo() {
+    qNo.innerHTML = parseInt(qNo.innerHTML) + 1;
+}
+
+// Update score for correct answers
+function updateScore() {
+    score.innerHTML = parseInt(score.innerHTML) + 10; // Add 10 marks for correct answers
+}
+
+// Display message at the end
+function lastMessage() {
+    if (fScore.innerText >= 80) {
+        message.innerHTML = "WOW !! UNBELIEVABLE !! &#128525;";
+    } else if (fScore.innerText >= 50) {
+        message.innerHTML = "TOO CLOSE !! &#128531;";
+    } else if (fScore.innerText >= 20) {
+        message.innerHTML = "Better luck next time &#128549;";
+    } else {
+        message.innerHTML = "Bad Luck &#128577;";
+    }
+}
+
+// Timer logic
 function timed() {
+    let timeLeft = 60; // Total time in seconds
+    progress.style.width = "100%"; // Reset the progress bar width to full
+
     t = setInterval(() => {
-        progress.style.width = (parseInt(progress.style.width) - 1) + "%";
-        console.log("called");
-        if (parseInt(progress.style.width) == 0) {
+        timeLeft--;
+        progress.style.width = (timeLeft / 60) * 100 + "%"; // Adjust width proportionally
+
+        if (timeLeft <= 0) {
             clearInterval(t);
             nextQuestion();
         }
-    }, 100)
+    }, 1000); // Update every second
 }
 
-buttons[0].addEventListener('click', () => {
-    if (buttons[0].innerText == answer) {
-        doWhenCorrect(0);
-    } else {
-        doWhenIncorrect(0);
-    }
-    clearInterval(t);
-    outro(0);
-});
-buttons[1].addEventListener('click', () => {
-    if (buttons[1].innerText == answer) {
-        doWhenCorrect(1);
-    } else {
-        doWhenIncorrect(1);
-    }
-    clearInterval(t);
-    outro(1);
-});
-buttons[2].addEventListener('click', () => {
-    if (buttons[2].innerText == answer) {
-        doWhenCorrect(2);
-    } else {
-        doWhenIncorrect(2);;
-    }
-    clearInterval(t);
-    outro(2);
-});
-buttons[3].addEventListener('click', () => {
-    if (buttons[3].innerText == answer) {
-        doWhenCorrect(3);
-    } else {
-        doWhenIncorrect(3);
-    }
-    clearInterval(t);
-    outro(3);
-});
+// Button click events
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        clearInterval(t); // Stop the timer
+        if (parseInt(buttons[i].innerHTML) === answer) {
+            buttons[i].style.backgroundColor = "green";
+            buttons[i].style.color = "#fff";
+            updateScore(); // Update the score if correct
+        } else {
+            buttons[i].style.backgroundColor = "red";
+            buttons[i].style.color = "#fff";
+        }
+
+        setTimeout(nextQuestion, 2000); // Delay for 2 seconds
+    });
+}
